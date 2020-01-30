0ce0718597c3620304813285c2212aa770c50df0
Kirill Kiselev <skulpin@mail.ru>
2020-01-30 18:38:32 +0700

    добавлена функция main

--- script.js
+++ script.js
@@ -4,43 +4,51 @@ let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-function tryEnter() {
-  let userNumber = prompt('Угадай число от 1 до 100', 20);
 
-  let ask = false;
-  let message;
-  let number = 30;
-
-  if (userNumber === null) {
-    alert('Игра закончена');
-
-    return false;
-  }
-
-  if (userNumber > number) {
-    ask = true;
-    message = 'Загаданное число меньше';
-  }
-
-  if (userNumber < number) {
-    ask = true;
-    message = 'Загаданное число больше';
-  }
-
-  if (+userNumber === number) {
-    message = 'Вы угадали!';
-  }
-
-  if (!isNumber(userNumber)) {
-    ask = true;
-    message = 'Введи число!';
+function main() {
+  let number = Math.ceil(Math.random() * 100);
+  console.log('number: ', number);
+  
+  function tryEnter() {
+    let userNumber = prompt('Угадай число от 1 до 100', 20);
+    
+    let message;
+    
+    let ask = false;
+    
+    if (userNumber === null) {
+      alert('Игра закончена');
+      
+      return false;
+    }
+    
+    if (userNumber > number) {
+      ask = true;
+      message = 'Загаданное число меньше';
+    }
+    
+    if (userNumber < number) {
+      ask = true;
+      message = 'Загаданное число больше';
+    }
+    
+    if (+userNumber === number) {
+      message = 'Вы угадали!';
+    }
+    
+    if (!isNumber(userNumber)) {
+      ask = true;
+      message = 'Введи число!';
+    }
+    
+    alert(message);
+    
+    if (ask === true) {
+      tryEnter();
+    }
   }
   
-  alert(message);
-
-  if (ask === true) {
-    tryEnter();
-  }
+  tryEnter();
 }
 
-tryEnter();
+main();
